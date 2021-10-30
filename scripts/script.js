19f7709dbe88235c00c8adc18c440345976bb8c7
Kajvd <58260553+Kajvd@users.noreply.github.com>
2021-10-30 14:05:20 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-var hamburger = document.querySelector(".hamburger");
-var navMenu = document.querySelector(".nav-menu");
+var hamburger = document.querySelector(".hamburger");       /* maakt een variabele van de geselecteerde class die in het html bestand staat. */ 
+var navMenu = document.querySelector(".nav-menu");          /* maakt een variabele van de geselecteerde class die in het html bestand staat. */ 
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", mobileMenu);            /* bij de event 'click' leest hij over een geklikt is op dit item, zo ja speelt de functie mobileMenu af */ 
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+function mobileMenu() {                                     /* hier word voor de functie mobileMenu geschreven wat er moet gebeuren */
+    hamburger.classList.toggle("active");                   /* hier word hamburger "active" gezet en word er active geplakt achter de class zodat er dus een nieuwe class ontstaat waar in css nieuwe waardes aan vast kunnen zitten */
+    navMenu.classList.toggle("active");                     /* hier word navMenu "active" gezet en word er active geplakt achter de class zodat er dus een nieuwe class ontstaat waar in css nieuwe waardes aan vast kunnen zitten */
 }
